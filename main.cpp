5e94a47431f1fae41072750dd1a748634d109767
DimaKolesnyk <dimakoless@gmail.com>
2015-09-30 22:51:39 +0300

    add margeSort

--- main.cpp
+++ main.cpp
@@ -8,7 +8,9 @@ int main() {
     std::vector <double> data = {2.345, 22, 0.4, -4, 34.2, 0.00442};
 
     //Run insertion Sort algorithms
-    algo::insertionSort(data);
+    //algo::insertionSort(data);
+
+    algo::margeSort(data);
 
     //View result
     algo::viewData(data);
